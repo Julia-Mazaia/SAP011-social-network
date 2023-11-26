97f4bb4b12052552753c0346495b23d51fc77a22
Julia Mazaia <140029817+Julia-Mazaia@users.noreply.github.com>
2023-11-26 12:24:40 -0300

    alterações pagina home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,11 +3,9 @@ export default () => {
 
     const template = `<main id="main" class="main"> 
     <div class="newPost">
-      <div class="infoUser">
-        <div class="imageUser"></div>
-        <srtong> Usuário </srtong>
-      </div>
-      <form action="">
+      <form class ="formPost">
+      <div class="imageUser"></div>
+        <strong>Usuário da Silva </strong>
         <textarea name="textarea" placeholder="O que você está pensando?"></textarea>
         <div class="iconsAndButton">
           <div class="icons">
@@ -19,6 +17,28 @@ export default () => {
         </div>  
       </form>
     </div>
+
+    <ul class= "posts">
+    <li class= "post">
+    <div class= "infoUserPost">
+    <div class= "imgUserPost"> </div> 
+    <div class= "nameAndHour">
+    <strong> Usuário da Silva </strong>
+    <p> 21h </p>
+    </div>
+    </div>
+    
+    <p> Mussum Ipsum, cacilds vidis litro abertis. Per aumento de cachacis, eu reclamis. Praesent vel viverra nisi. 
+    Mauris aliquet nunc non turpis scelerisque, eget.
+    Quem num gosta di mim que vai caçá sua turmis! Morbi viverra placerat justo, vel pharetra turpis. </p>
+
+    <div class= "actionBtnPost">
+    <button type="button" class= "filesPost"> <img src= " "//insira imagem aqui alt="Curtir"> </button>
+    <button type="button" class= "filesPost"> <img src= " "//insira imagem aqui alt="Comentar"> </button>
+    <button type="button" class= "filesPost"> <img src= " "//insira imagem aqui alt="Compartilhar"> </button>
+    </div>
+
+
   </main> `;
     
     
